96b9131af9249252a3972220b79a8b2eebbc2327
Steven <java20131114@gmail.com>
2023-04-30 05:43:56 +0800

    ✨ feat: 404 Page

--- web/src/views/error/404.vue
+++ web/src/views/error/404.vue
@@ -1,4 +1,23 @@
 <template>
+    <div class="text-center">
+        <el-header>
+            <TopMenu/>
+        </el-header>
 
-    <h1>404</h1>
+
+        <div class="pt-20">
+            <Lottie :height="250" name="Thinking-face"/>
+        </div>
+        <div class="py-5">
+            <h1>找不到你想要的东西</h1>
+        </div>
+        <div class="text-base">
+            <router-link to="/">回到首页</router-link>
+        </div>
+
+    </div>
 </template>
+<script setup>
+import TopMenu from "../../components/Menu/TopMenu.vue";
+import Lottie from "../../components/Lottie.vue";
+</script>
